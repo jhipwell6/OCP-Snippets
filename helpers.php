0364f646f226c0dac62062e777888933bfbc6a35
Jason <jason@oldcitypress.com>
2015-08-04 20:45:09 -0500

    Update helpers.php
    included function descriptions

--- helpers.php
+++ helpers.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * link_target()
+ * params = $url (string)
+ * return '' || target="_blank"
+ */
 function link_target($url) {
 	$extension = pathinfo($url, PATHINFO_EXTENSION);
 	$is_doc = array('doc','docx','pdf','ppt','pptx','xls','xlsx','zip');
@@ -11,6 +16,11 @@ function link_target($url) {
 	return $target;
 }
 
+/*
+ * get_col_x()
+ * params = $arr (array), $size (string)
+ * return col-size-#
+ */
 function get_col_x($arr, $size) {
 	$sz = $size == '' ? 'sm' : $size;
 	$col = '';
@@ -53,6 +63,11 @@ function get_col_x($arr, $size) {
 	return $col;
 }
 
+/*
+ * ocp_url_type()
+ * params = $url (string)
+ * return 'youtube' || 'vimeo' || 'file'
+ */
 function ocp_url_type($url) {
     if (strpos($url, 'youtube') > 0 || strpos($url, 'youtu') > 0) {
         return 'youtube';
@@ -63,6 +78,11 @@ function ocp_url_type($url) {
     }
 }
 
+/*
+ * ocp_video_id()
+ * params = $url (string)
+ * return id
+ */
 function ocp_video_id($url) {
 	$type = ocp_url_type($url);
 	if($type == 'file') {
@@ -91,13 +111,27 @@ function ocp_video_id($url) {
 }
 
 
-/* FIELDS */
+/*
+ * ACF FIELD HELPERS
+ * dependency = advanced-custom-fields-pro
+ */
+ 
+/*
+ * the_conditional_field()
+ * params = $name (string), $before (string), $after (string), $sub (bool)
+ * echo $before . $value . $after
+ */
 function the_conditional_field($name, $before, $after, $sub = false) {
 	$value = $sub !== false ? get_sub_field($name) : get_field($name);
 	if($value)
 		echo $before . $value . $after;
 }
 
+/*
+ * the_image()
+ * params = $name (string), $format (string), $sub (bool)
+ * echo $value[$format]
+ */
 function the_image($name = '', $format = 'url', $sub = false) {
 	$value = $sub !== false ? get_sub_field($name) : get_field($name);
 	if($value) {
@@ -105,6 +139,11 @@ function the_image($name = '', $format = 'url', $sub = false) {
 	}
 }
 
+/*
+ * the_video()
+ * params = $name (string), $format (string), $sub (bool)
+ * echo embed || image || url || id
+ */
 function the_video($name = '', $format = 'id', $sub = false) {
 	$url = $sub !== false ? get_sub_field($name) : get_field($name);
 	$type = ocp_url_type($url);
@@ -130,6 +169,11 @@ function the_video($name = '', $format = 'id', $sub = false) {
 	}
 }
 
+/*
+ * the_background()
+ * params = $name (string), $option (bool)
+ * echo color || image
+ */
 function the_background($name, $option = false) {
 	$repeater = $option == false ? get_field($name) : get_field($name, 'option');
 	$row = $repeater[0];
